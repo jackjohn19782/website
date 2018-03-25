a01951e375793de52d061f5a514789b91dbc8808
jackjohn19782 <37772192+jackjohn19782@users.noreply.github.com>
2018-03-25 14:52:50 +0300

    Removed Password
    Removed the password from wp-config file to prevent hackers

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'website_users');
 define('DB_USER', 'uursprx_user');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Aa123456');
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
 define('DB_HOST', '13.95.125.0');
